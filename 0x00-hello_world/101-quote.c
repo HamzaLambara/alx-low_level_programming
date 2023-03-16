925ab088095b08cc96da260e9c0ebbfa67081f7a
HamzaLambara <lambara.hamza@gmail.com>
2023-03-16 14:56:42 -0700

    My first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19, followed by a new line, to the standard error.
+ *
+ * Return: 1
+ */
+int main(void)
+{
+    /* Prints the message to the standard error stream */
+    write(2,
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+        59);
+
+    return (1);
+}
