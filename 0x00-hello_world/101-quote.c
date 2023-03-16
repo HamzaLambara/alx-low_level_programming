c79e8800442b625324d34e98b7591dffd8a0ee7d
HamzaLambara <lambara.hamza@gmail.com>
2023-03-16 14:59:38 -0700

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,12 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: Prints "and that piece of art is useful" - Dora Korpar,
- * 2015-10-19, followed by a new line, to the standard error.
- *
- * Return: 1
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-    /* Prints the message to the standard error stream */
-    write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-        59);
-
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
