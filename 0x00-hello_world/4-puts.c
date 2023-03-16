6e702084a139a6bea17839d5691ad0698886b9f8
HamzaLambara <lambara.hamza@gmail.com>
2023-03-16 14:27:44 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: This program prints the message
- * "Programming is like building a multilingual puzzle"
- * using the puts function.
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
