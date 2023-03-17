8fa85ee088ccb36d81803c5ee9246959b907f183
HamzaLambara <lambara.hamza@gmail.com>
2023-03-17 14:43:32 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar('\n');
+return (0);
+}
