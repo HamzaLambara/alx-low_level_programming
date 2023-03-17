f45e15cc75e7a33cc7099aa7b9b7b4b7b28d09ef
HamzaLambara <lambara.hamza@gmail.com>
2023-03-17 14:45:34 -0700

    My first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ *Program prints the numbers from 0 to 9 separated by a comma
+ * and a space using only the putchar function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int num = 0;
