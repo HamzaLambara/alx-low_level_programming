2defe5cdaa745f68958204c73db17951b54d45f0
HamzaLambara <lambara.hamza@gmail.com>
2023-03-17 14:54:02 -0700

    My first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ for (i = 0; i < 8; i++)
 {
 for (j = i + 1; j < 9; j++)
 {
-for (k = j + 1; k < 10;k++) 
+for (k = j + 1; k < 10; k++) 
 {
 putchar(i + '0');
 putchar(j + '0');
@@ -25,6 +25,5 @@ putchar(' ');
 }
 }
 }
-putchar('\n');
 return (0);
 }
