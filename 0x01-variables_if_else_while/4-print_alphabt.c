09ca037e380632f4dacca0639a15b975042735e2
HamzaLambara <lambara.hamza@gmail.com>
2023-03-17 14:00:16 -0700

    My first commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * This program prints the alphabet in lowercase, excluding the letters 'e' and 'q'.
+ *Program prints the alphabet in lowercase excluding the letters 'e' and 'q'
  *
  * Return: Always 0 (Success)
  */
