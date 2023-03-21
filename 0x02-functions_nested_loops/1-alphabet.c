715b255dd58de25c982c7e7b8de8b28651a1dd18
HamzaLambara <lambara.hamza@gmail.com>
2023-03-21 07:12:24 -0700

    My first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the lowercase alphabet
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
  */
 void print_alphabet(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
+letter = 'a';
+while (letter <= 'z')
+{
+if (letter == 'e' || letter == 'q')
+{
+letter++;
+continue;
+}
 _putchar(letter);
+letter++;
+}
 _putchar('\n');
 }
