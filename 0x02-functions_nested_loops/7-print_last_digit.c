37791c44c15712c1d3525d5a3e6d7adcaa9e4fa2
HamzaLambara <lambara.hamza@gmail.com>
2023-03-21 17:17:15 -0700

    My first commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,6 @@
  *
  * Return: The value of the last digit
  */
-/**
- * print_last_digit - Prints the last digit of a number
- * @n: The number whose last digit to print
- *
- * Return: The value of the last digit
- */
 int print_last_digit(int n)
 {
 int last_digit;
