747a2c393aab2cadf84be2ded84a92dd69d860fd
HamzaLambara <lambara.hamza@gmail.com>
2023-03-22 18:37:14 -0700

    My first commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-
 /**
- * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0 (Success)
  */
