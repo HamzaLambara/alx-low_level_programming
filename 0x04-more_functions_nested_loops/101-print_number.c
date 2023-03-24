b642d555ec4a45eef644ec83fbc9ebefef56de9d
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 09:11:30 -0700

    My first commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,51 +2,22 @@
 #include <math.h>
 
 /**
- * main - Finds and prints the largest prime factor of 612852475143
- *
- * Return: Always 0.
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
+ * Return: 0
  */
 int main(void)
 {
-long int n = 612852475143;
-long int largest = 1;
-long int i;
-
-while (n % 2 == 0)
+int c;
+long num = 612852475143;
+for (c = (int) sqrt(num); c > 2; c++)
 {
-largest = 2;
-n /= 2;
-}
-
-for (i = 3; i <= sqrt(n); i += 2)
+if (num % c == 0)
 {
-while (n % i == 0)
-{
-largest = i;
-n /= i;
+printf("%d\n", c);
+break;
 }
 }
-
-if (n > 2)
-{
-largest = n;
-}
-
-/* Print the largest prime factor using putchar() */
-if (largest < 0)
-{
-putchar('-');
-largest = -largest;
-}
-
-if (largest / 10)
-{
-main(largest / 10);
-}
-
-putchar(largest % 10 + '0');
-
 return (0);
-
 }
-
