5716d65f6fdccdcca6aee3abfa72638a98076d39
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 07:43:34 -0700

    My first commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,10 @@
 
 /**
  * print_line - Draws a straight line in the terminal
- * @n: The number of times the character _ should be printed
+ *
+ * @n: Number of times the character '_' should be printed
+ *
+ * Return: void
  */
 void print_line(int n)
 {
@@ -17,6 +20,6 @@ for (i = 0; i < n; i++)
 _putchar('_');
 }
 _putchar('\n');
-    
+
 }
 
