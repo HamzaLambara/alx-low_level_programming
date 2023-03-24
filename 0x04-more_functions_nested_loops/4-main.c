4b0e34753be4f08e25224b7100b0a338be041b2d
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 07:02:20 -0700

    My first commit

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * main - Entry point
+ *
+ * Calls the function to print numbers from 0 to 9 except 2 and 4
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 print_most_numbers();
