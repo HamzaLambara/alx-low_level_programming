0484867d77c07105c2369b8b53c276053379449d
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 07:04:19 -0700

    My first commit

--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - check the code
  *
- * Calls the function to print numbers from 0 to 9 except 2 and 4
- *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
