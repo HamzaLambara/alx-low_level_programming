1f5e125d263a5f17a39ae19b3e5c9e815670cdbb
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 08:23:59 -0700

    My first commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,59 @@
-#include <stdio.h>
-
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
-printf("FizzBuzz ");
-} else if (i % 3 == 0) {
-printf("Fizz ");
-} else if (i % 5 == 0) {
-printf("Buzz ");
-} else {
-printf("%d ", i);
+int i, n;
+char num[3];
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+_putchar('B');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+}
+else if (i % 3 == 0)
+{
+_putchar('F');
+_putchar('i');
+_putchar('z');
+_putchar('z');
+}
+else if (i % 5 == 0)
+{
+_putchar('B');
+_putchar('u');
+_putchar('z');
+_putchar('z');
+}
+else
+{
+n = i;
+num[2] = (n % 10) + '0';
+n /= 10;
+num[1] = (n % 10) + '0';
+num[0] = (n / 10) + '0';
+if (i > 9)
+_putchar(num[0]);
+_putchar(num[1]);
+_putchar(num[2]);
+}
+if (i != 100)
+{
+_putchar(' ');
 }
 }
-
-printf("\n");
-
-return 0;
+_putchar('\n');
+return (0);
 
 }
 
