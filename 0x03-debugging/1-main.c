5b6fef4c1c8e2ad21b045a60537a3bcc7e66035a
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 18:31:11 -0700

    My first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,11 +9,8 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
 /*
 * while (i < 10)
 * {
@@ -21,7 +18,6 @@ i = 0;
 * }
 */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 
 }
