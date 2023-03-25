d317559c9562121fb9bcc548fb9a697fd5a6d3a7
HamzaLambara <lambara.hamza@gmail.com>
2023-03-24 18:56:47 -0700

    My first commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,30 +9,34 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-int days;
-if (month < 1 || month > 12)
+int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int day_of_year = 0;
+int i;
+for (i = 0; i < month - 1; i++)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
+day_of_year += days_in_month[i];
 }
-if (day < 1 || day > 31)
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-return;
-}
-days = convert_day(month, day);
+day_of_year += day;
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 {
 if (month > 2)
 {
-days++;
+day_of_year++;
 }
-printf("Day of the year: %d\n", days);
-printf("Remaining days: %d\n", 366 - days);
+}
+if (month > 12 || day > days_in_month[month - 1])
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
 }
 else
 {
-printf("Day of the year: %d\n", days);
-printf("Remaining days: %d\n", 365 - days);
+printf("Day of the year: %d\n", day_of_year);
+printf("Remaining days: %d\n", (is_leap_year(year) && day_of_year > 59) ? 366 - day_of_year : 365 - day_of_year);
 }
 }
+
+int is_leap_year(int year)
+{
+return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
