9b17bd32f14dbaf0f622266d216823154c2538c2
HamzaLambara <lambara.hamza@gmail.com>
2023-03-29 21:45:33 -0700

    My first commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,23 @@
 
 char *leet(char *s)
 {
-	int i;
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
+for (i = 0; s[i] != '\0'; i++)
+{
+j = 0;
+while (letters[j] != '\0')
+{
+if (s[i] == letters[j])
+{
+s[i] = numbers[j];
+break;
+}
+j++;
+}
+}
 
-	return (s);
+return s;
 }
