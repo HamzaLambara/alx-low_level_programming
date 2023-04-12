d2aa770f3ad950c9bcc65f80286cf26e3bc4fd44
HamzaLambara <lambara.hamza@gmail.com>
2023-04-11 18:35:23 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,21 +9,21 @@
  */
 int count_words(char *str)
 {
-        int i, count = 0, flag = 0;
+int i, count = 0, flag = 0;
 
-        for (i = 0; str[i]; i++)
-        {
-                if (str[i] != ' ')
-                {
-                        if (flag == 0)
-                                count++;
-                        flag = 1;
-                }
-                else
-                        flag = 0;
-        }
+for (i = 0; str[i]; i++)
+{
+if (str[i] != ' ')
+{
+if (flag == 0)
+count++;
+flag = 1;
+}
+else
+flag = 0;
+}
 
-        return (count);
+return (count);
 }
 
 /**
@@ -35,49 +35,49 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-        char **matrix, *tmp;
-        int i, j, k = 0, len = 0, words;
+char **matrix, *tmp;
+int i, j, k = 0, len = 0, words;
 
-        if (str == NULL || count_words(str) == 0)
-                return (NULL);
+if (str == NULL || count_words(str) == 0)
+return (NULL);
 
-        words = count_words(str);
-        matrix = malloc(sizeof(char *) * (words + 1));
+words = count_words(str);
+matrix = malloc(sizeof(char *) * (words + 1));
 
-        if (matrix == NULL)
-                return (NULL);
+if (matrix == NULL)
+return (NULL);
 
-        for (i = 0; str[i]; i++)
-        {
-                if (str[i] != ' ')
-                {
-                        len = 0;
+for (i = 0; str[i]; i++)
+{
+if (str[i] != ' ')
+{
+len = 0;
 
-                        for (j = i; str[j] && str[j] != ' '; j++)
-                                len++;
+for (j = i; str[j] && str[j] != ' '; j++)
+len++;
 
-                        tmp = malloc(sizeof(char) * (len + 1));
+tmp = malloc(sizeof(char) * (len + 1));
 
-                        if (tmp == NULL)
-                        {
-                                for (k = 0; k < i; k++)
-                                        free(matrix[k]);
+if (tmp == NULL)
+{
+for (k = 0; k < i; k++)
+free(matrix[k]);
 
-                                free(matrix);
-                                return (NULL);
-                        }
+free(matrix);
+return (NULL);
+}
 
-                        for (j = i; str[j] && str[j] != ' '; j++)
-                                tmp[j - i] = str[j];
+for (j = i; str[j] && str[j] != ' '; j++)
+tmp[j - i] = str[j];
 
-                        tmp[j - i] = '\0';
-                        matrix[k] = tmp;
-                        k++;
-                        i = j - 1;
-                }
-        }
+tmp[j - i] = '\0';
+matrix[k] = tmp;
+k++;
+i = j - 1;
+}
+}
 
-        matrix[k] = NULL;
-        return (matrix);
+matrix[k] = NULL;
+return (matrix);
 }
 
