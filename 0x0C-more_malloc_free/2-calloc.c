42b47c337dc9f616c82a7055badd601cdfde269a
HamzaLambara <lambara.hamza@gmail.com>
2023-04-13 19:48:40 -0700

    Malloc Second Part

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _calloc - allocates memory for an array given number of elements and size
@@ -9,17 +10,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
-	unsigned int i;
+char *p;
+unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
-		return (NULL);
+if (nmemb <= 0 || size <= 0)
+return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
 
-	return (p);
+for (i = 0; i < (nmemb * size); i++)
+p[i] = 0;
+
+return (p);
 }
+
