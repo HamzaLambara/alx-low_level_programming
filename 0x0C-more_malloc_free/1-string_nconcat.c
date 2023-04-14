1074a93e1ef89be9364f114a7385fa84748e4d8f
HamzaLambara <lambara.hamza@gmail.com>
2023-04-13 21:21:04 -0700

    Malloc Second Part

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p = malloc((strlen1 + n + 1) * sizeof(char));
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, c = 0; i <(int)(strlen1 + n); i++)
+	for (i = 0, c = 0; i < (int)(strlen1 + n); i++)
 	{
 		if (i < strlen1)
 			p[i] = s1[i];
